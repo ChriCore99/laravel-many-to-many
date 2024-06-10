f20f3d3d1d6cfe7bfe602fcd7ae19b5123383c2f
ChriCore99 <christian.corelli.io@gmail.com>
2024-06-10 17:37:11 +0200

    ADD: collegamento tabelle e model projects and technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,4 +12,8 @@ class Project extends Model
         return $this->hasMany(Type::class);
     }
 
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
+
 }
